b0104e90a0546c2371bbfeacb22fee46bed87141
valebalcit <valentinabalcazar@unicauca.edu.co>
2026-02-11 08:26:07 -0500

    feat(db): try connection sqlite

---
+++ ProyectoFinal_IngS_II/src/main/java/DataBase/ConexionSQLite.java
@@ -0,0 +1,21 @@
+package DataBase;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionSQLite {
+    private static final String URL = "jdbc:sqlite:piedraAzul.db";
+
+    public static Connection conectar() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(URL);
+            System.out.println("Conexión exitosa a SQLite");
+        } catch (SQLException e) {
+            System.out.println("Error de conexión: " + e.getMessage());
+        }
+        return conn;
+    }
+    
+}
