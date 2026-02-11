9466406bf982c9fb91c6d9b4a308cc229a81d313
valebalcit <valentinabalcazar@unicauca.edu.co>
2026-02-11 08:11:53 -0500

    feat(initial): create project with maven

---
+++ ProyectoFinal_IngS_II/src/main/java/com/mycompany/proyectofinal_ings_ii/ProyectoFinal_IngS_II.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectofinal_ings_ii;
+
+/**
+ *
+ * @author valeb
+ */
+public class ProyectoFinal_IngS_II {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
