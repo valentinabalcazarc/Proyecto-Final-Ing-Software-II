86d6b45de673362857c530fd3834d46ea2b19510
Sam <samuelvillaquiran@unicauca.edu.co>
2026-02-15 19:38:13 -0500

    feat (back): UserService
    Include: register user and authenticate user

--- ProyectoFinal_IngS_II/src/main/java/services/UserService.java
+++ ProyectoFinal_IngS_II/src/main/java/services/UserService.java
@@ -1,9 +1,29 @@
 package services;
 
+import repository.UserRepository;
+import configuration.IAutehnticationService;
+import models.User;
+
 public class UserService {
-
-   
     
+    private UserRepository userRepository;
+    private IAutehnticationService authService;
 
-    
+    public UserService(UserRepository userRepository, IAutehnticationService authService) {
+        this.userRepository = userRepository;
+        this.authService = authService;
+    }
+
+    public boolean regUser(User newUser) {
+        String hash = authService.encrypt(newUser.getPassUser());
+        newUser.setPassUser(hash);
+        userRepository.save(newUser);
+        return true;
+    }
+
+    public boolean authUser(int cedUser, String password) {
+        User user = userRepository.findByCedUser(cedUser);
+        return authService.verify(user.getPassUser(),password);
+    }
+
 }
